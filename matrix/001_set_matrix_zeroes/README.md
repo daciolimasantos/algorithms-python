84dd4171478f08bdce1c7332415c091af464e9e0
daciolimasantos <daciolimasantos@gmail.com>
2026-03-17 12:52:56 -0300

    add set matrix zeroes

---
+++ matrix/001_set_matrix_zeroes/README.md
@@ -0,0 +1,30 @@
+# Set Matrix Zeroes
+
+## Problem
+
+Set row and column to zero.
+
+
+## Idea
+
+Store rows and cols.
+
+
+## Algorithm
+
+find zeros
+mark rows
+mark cols
+update
+
+
+## Complexity
+
+Time: O(m*n)
+
+Space: O(m+n)
+
+
+## Code
+
+See solution.py
---
+++ matrix/001_set_matrix_zeroes/soution.py
@@ -0,0 +1,21 @@
+class SOlution:
+    def setZeroes(self, matrix):
+        
+        rows = set()
+        cols = set()
+        
+        m = len(matrix)
+        n = len(matrix[0])
+        
+        for r in range(m):
+            for c in range(n):
+                
+                if matrix[r][c] == 0:
+                    rows.add(r)
+                    cols.add(c)
+                    
+        for r in range(m):
+            for c in range(n):
+                
+                if r in rows or c in cols:
+                    matrix[r][c] = 0
